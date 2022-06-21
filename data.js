300e539092e95256130184d26c432bb1811e23b1
halilesmer <halil@esmer.de>
2022-06-21 08:20:02 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const results= [
+    {
+        "id": 654959,
+        "title": "Pasta With Tuna",
+        "image": "https://spoonacular.com/recipeImages/654959-312x231.jpg",
+        "imageType": "jpg",
+        "nutrition": {
+            "nutrients": [
+                {
+                    "name": "Fat",
+                    "amount": 10.3277,
+                    "unit": "g"
+                }
+            ]
+        }
+    },
+    {
+        "id": 654812,
+        "title": "Pasta and Seafood",
+        "image": "https://spoonacular.com/recipeImages/654812-312x231.jpg",
+        "imageType": "jpg",
+        "nutrition": {
+            "nutrients": [
+                {
+                    "name": "Fat",
+                    "amount": 3.97495,
+                    "unit": "g"
+                }
+            ]
+        }
+    }
+]
