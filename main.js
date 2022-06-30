a93d40b30011fc338e514740aa07b99f05fd3ced
halilesmer <halil@esmer.de>
2022-06-30 11:13:15 +0200

    some inprovements and refactoring + Warning alert for searching

--- main.js
+++ main.js
@@ -12,178 +12,136 @@ const fetchIngredientsDatas = async () => {
 
 const controller = async () => {
   try {
-    const data = await fetchIngredientsDatas();
+    const data =
+      (await fetchIngredientsDatas()) !== undefined
+        ? fetchIngredientsDatas()
+        : localIngredientsData;
     console.log("data: ", data);
 
-    // if the data not reachable, than use local data
-    data === undefined
-      ? cards(localIngredientsData)
-      : cards(data)
-
-
-
-
-    searchFunc()
-
-
-
+    // cards(data);
+    cards(data);
+    recipeDetailsEventListener(data);
+    searchFunc(data);
   } catch (error) {
     console.log("error: ", error);
   }
-
-
-
 };
-
-
-
 
 const cards = (data) => {
+  const dataLength = data.length;
+  console.log("dataLength: ", dataLength);
   const cardContainer = document.querySelector(".card-container");
+  console.log("cardContainer: ", cardContainer);
+  
 
   const cardRow = document.querySelector("#card-lists .row");
+  /* first delete the content of card elements */
+  cardRow.innerHTML = "";
 
-  for (let i = 0; i < data.length; i++) {
-    cardRow.insertAdjacentHTML(
-      "afterbegin",
+
+
+  if (dataLength >= 1) {
+        for (let i = 0; i < data.length; i++) {
+          cardRow.insertAdjacentHTML(
+            "afterbegin",
+            `
+      <div class="col">
+            <div class="card" style="width: 18rem;"">
+  
+              <div class="image-wrapper hover01 column">
+                <figure><img src="${
+                  data[i].image
+                }" class="card-img-top" alt="...">
+                </figure>
+              </div>
+  
+              <div class="card-body">
+                <h3 class="card-title">${data[i].title}</h3>
+              </div>
+              <div class="list-group list-group-flush">
+                <i class="fa fa-star checked"></i>
+                <i class="fa fa-star checked"></i>
+                <i class="fa fa-star checked"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+              </div>
+              <div class="list-group list-group-flush">
+                 <i class="fa-solid fa-clock"></i>
+  <span>  ${
+    recipeDetail.preparationMinutes + recipeDetail.cookingMinutes
+  }  Min</span>
+              </div>
+              <div class="list-group list-group-flush">
+                <a class="btn btn-outline-info card-link" href="#" role="button">Recipe Details</a>
+  
+              </div>
+          </div>
+     </div>
+      
+  
       `
-    <div class="col">
-          <div class="card" style="width: 18rem;"">
+          );
+        }
 
-            <div class="image-wrapper hover01 column">
-              <figure><img src="${data[i].image
-      }" class="card-img-top" alt="...">
-              </figure>
-            </div>
 
-            <div class="card-body">
-              <h3 class="card-title">${data[i].title}</h3>
-            </div>
-            <div class="list-group list-group-flush">
-              <i class="fa fa-star checked"></i>
-              <i class="fa fa-star checked"></i>
-              <i class="fa fa-star checked"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-            </div>
-            <div class="list-group list-group-flush">
-               <i class="fa-solid fa-clock"></i>
-<span>  ${recipeDetail.preparationMinutes + recipeDetail.cookingMinutes
-      }  Min</span>
-            </div>
-            <div class="list-group list-group-flush">
-              <a class="btn btn-outline-info card-link" href="#" role="button">Recipe Details</a>
+      } else {
+        console.log("else");
 
-            </div>
-        </div>
-   </div>
-    
+        const warningParagr = document.createElement("h2");
 
-    `
+        warningParagr.innerText =
+          "no results found. please try another search".toUpperCase();
+        cardRow.appendChild(warningParagr);
+      }
+  
+  
+  
+  
+    };
+  
+
+function searchFunc(data) {
+  const input = document.getElementById("searchInput");
+
+  return input.addEventListener("input", (e) => {
+    /* multiple spaces trimmed */
+    let inputValue = e.target.value.trim().replace(/  +/g, " ").toUpperCase();
+    console.log("inputValue: ", inputValue);
+
+    const searchResult = data.filter((item) =>
+      item.title.toUpperCase().includes(inputValue)
     );
-  }
-  searchFunc()
-
-
-  cardContainer.addEventListener("click", () => {
-    recipeDetailsEventListener(data);
+    return cards(searchResult);
   });
-
-};
-
-function searchFunc() {
-  const cardContainer = document.querySelector(".card-container");
-  const cardLists = document.querySelector("#card-lists");
-  const input = document.getElementById("searchInput").value.toUpperCase();
-  console.log("input: ", input);
-
-  const cards = cardLists.getElementsByClassName("col");
-
-  const warningParagr = document.createElement('p');
-  warningParagr.innerText = ''
-  cardContainer.appendChild(warningParagr)
-
-  console.log('test', !'Buffalo Chicken Wings Wonton Wraps'.toUpperCase().includes(input))
-
-
-  for (let i = 0; i < cards.length; i++) {
-    let title = cards[i].querySelector(".card-body h3.card-title");
-
-    if (title.innerText.toUpperCase().indexOf(input) > -1) {
-      cards[i].style.display = "";
-      console.log('if')
-    } else {
-      cards[i].style.display = "none";
-      console.log('else', cards.length)
-    }
-  }
-
-  // if (!(title.textContent.toUpperCase().includes(input)) && (input.length > 0)) {
-  //   console.log('else if')
-  //   warningParagr.innerText = ''
-
-  //   warningParagr.innerText = 'no results found. please try another search'.toUpperCase()
-  //   cardContainer.appendChild(warningParagr)
-  // }
-  //  else if ((title.innerText.toUpperCase() === input) && (input.length > 0)) {
-  //   console.log('else if')
-  //   warningParagr.innerText = ''
-  //   cardContainer.appendChild(warningParagr)
-  // } 
+ 
 }
 
 
-
-  /* works welll!!! /// searchFunc()*/
-  // const cardContainer = document.querySelector(".card-container");
-  // const cardLists = document.querySelector("#card-lists");
-  // const input = document.getElementById("searchInput").value.toUpperCase();
-  // console.log("input: ", input);
-
-  // const cards = cardLists.getElementsByClassName("col");
-
-  // const warningParagr = document.createElement('p');
-  // warningParagr.innerText = '' 
-  // cardContainer.appendChild(warningParagr)
-
-  // console.log('test', !'Buffalo Chicken Wings Wonton Wraps'.toUpperCase().includes(input))
-
-
-  // for (let i = 0; i < cards.length; i++) {
-  //   let title = cards[i].querySelector(".card-body h3.card-title");
-
-  //   if (title.innerText.toUpperCase().indexOf(input) > -1) {
-  //     cards[i].style.display = "";
-  //     console.log('if')
-  //   }  else {
-  //     cards[i].style.display = "none";
-  //     console.log('else', cards.length)
-  //   }
-  // }
-
-
-
-
-
-
-/* Open the Recipe Details Site */
-
+/* --- Recipe Event Listener / Open the Recipe Details Site  -----*/
 const recipeDetailsEventListener = (data) => {
+  
+  const card = document.querySelector(".card");
+    
+      card.addEventListener("click", () => {
+        recipeDetails(data);
+      });
+}
+/* Recipe Details Site */
+
+const recipeDetails = (data) => {
   const cardContainer = document.querySelector(".card-container");
   cardContainer.classList.add("makeDisplayNone");
 
   const mainContainer = document.querySelector("main");
   mainContainer.classList.remove("makeDisplayNone");
 
-
-
   /* --- Selected Elements --- */
   const main = document.querySelector("main");
 
   /* --- Created Elements --- */
-  const recipeDetailsCon = document.createElement('div');
+  const recipeDetailsCon = document.createElement("div");
 
-  const backButtonDiv = document.createElement('div');
+  const backButtonDiv = document.createElement("div");
   const arrowLeftIcon = document.createElement("i");
   const backButton = document.createElement("button");
 
@@ -215,10 +173,13 @@ const recipeDetailsEventListener = (data) => {
 
   const moreButton = document.createElement("button");
   /* --- Set Attributes --- */
-  recipeDetailsCon.setAttribute('class', 'row justify-content-md-center mt-3 mb-2 indexMainContainer' )
+  recipeDetailsCon.setAttribute(
+    "class",
+    "row justify-content-md-center mt-3 mb-2 indexMainContainer"
+  );
 
-  backButtonDiv.setAttribute('class', 'arrow-left')
-  backButton.setAttribute('class', 'btn btn-primary btn-sm');
+  backButtonDiv.setAttribute("class", "arrow-left");
+  backButton.setAttribute("class", "btn btn-primary btn-sm");
   arrowLeftIcon.setAttribute("class", "fa-regular fa-left");
 
   recipeImageCon.setAttribute("class", "col col-md-4 recipeImageCon");
@@ -248,7 +209,7 @@ const recipeDetailsEventListener = (data) => {
   moreButton.setAttribute("id", "showMoreBtn");
 
   //-- Textcontent ----
-  backButton.textContent = 'Back';
+  backButton.textContent = "Back";
 
   recipeH1.textContent = recipeDetail.title;
   timeTextP.innerText = "Make it in: 35 minutes";
@@ -264,9 +225,8 @@ const recipeDetailsEventListener = (data) => {
   recipeDetailsCon.appendChild(backButtonDiv);
   backButtonDiv.appendChild(backButton);
   backButtonDiv.appendChild(arrowLeftIcon);
-  
+
   recipeDetailsCon.appendChild(recipeImageCon);
-  
 
   recipeImageCon.appendChild(mainImg);
 
@@ -333,20 +293,18 @@ const recipeDetailsEventListener = (data) => {
     showMoreFunc();
   }
 
-backButton.addEventListener('click', goHome)
+  backButton.addEventListener("click", goHome);
 };
 
-const goHome =()=>{
+const goHome = () => {
   const cardContainer = document.querySelector(".card-container");
   const mainContainer = document.querySelector("main");
 
-  console.log('goHome')
+  console.log("goHome");
   mainContainer.classList.add("makeDisplayNone");
   cardContainer.classList.remove("makeDisplayNone");
   // cardContainer.classList.add("makeDisplayNone");
-
-}
-
+};
 
 // Show more or less textes ---------------------------
 
