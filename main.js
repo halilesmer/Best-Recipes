d3ef3cc7acd2e56e66c9ad8c45737444b79d40ab
halilesmer <halil@esmer.de>
2022-07-05 17:50:04 +0200

    refactoring + added select options

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 const localIngredientsData = GroceryProductsData;
 
 const fetchIngredientsDatas = async () => {
-  let url =
-    `https://api.spoonacular.com/food/products/search?number=100&query=pizza&addProductInformation=true&apiKey=99675d45f6a34d1ebbba0e30a2c17fa3`;
+  let url = `https://api.spoonacular.com/food/products/search?number=100&query=pizza&addProductInformation=true&apiKey=99675d45f6a34d1ebbba0e30a2c17fa3`;
   // let url = './data/findByIngredients.js'
 
   const response = await fetch(url);
@@ -15,29 +14,25 @@ const fetchIngredientsDatas = async () => {
 /* ------- Controller ----- */
 const controller = async () => {
   try {
-    const data =
-      (await fetchIngredientsDatas()) !== undefined
-        ? fetchIngredientsDatas()
-        : localIngredientsData;
-    console.log("data: ", data);
+    // const data =
+    //   (await fetchIngredientsDatas()) !== undefined
+    //     ? fetchIngredientsDatas()
+    //     : localIngredientsData;
+    // console.log("data: ", data);
 
-    // deleteFilterButton(data);
+    const data = localIngredientsData;
+
+    deleteFilterButtonFunk(data);
     cards(data);
-    // recipeDetailsEventListener(data);
-    // searchFunc(data);
-    // checkBoxLikes(data);
+   
     addEventListener(data);
+getDataForOptions(data)
   } catch (error) {
     console.log("error: ", error);
   }
 };
 
-// const printData = (data) => {
-//   // recipeDetailsEventListener(data);
 
-//   // checkBoxLikes(data);
-//   cards(data);
-// };
 
 /* -------  Cards Lists ----------- */
 const cards = (data) => {
@@ -45,18 +40,19 @@ const cards = (data) => {
   /* ----- first delete the content of card elements -----*/
   cardRow.innerHTML = "";
 
-/* -------- Warning if no result displayed ---------- starts*/
-  if (data.length <1) {
+  /* -------- Warning if no result displayed ---------- starts*/
+  if (data.length < 1) {
     const warningParagr = document.createElement("h2");
-    warningParagr.innerText = "no results found. please try another search".toUpperCase();
+    warningParagr.innerText =
+      "no results found. please try another search".toUpperCase();
     cardRow.appendChild(warningParagr);
   }
   /* -------- Warning if no result displayed ---------- ends*/
 
-    for (let i = 0; i < data.length; i++) {
-      cardRow.insertAdjacentHTML(
-        "afterbegin",
-        `
+  for (let i = 0; i < data.length; i++) {
+    cardRow.insertAdjacentHTML(
+      "afterbegin",
+      `
       <div class="col mt-3 mb-3">
             <div class="card">
   
@@ -72,13 +68,17 @@ const cards = (data) => {
               </div>
               <div class="list-group list-group-flush likes">
               
-              ${data[i].badges.find(el => el === 'vegan')}
+               ${data[i].badges.find((el) => el === "vegan")}
 
 
               </div>
               <div class="list-group list-group-flush">
                  <i class="fa-solid fa-clock"></i>
-  <span>  ${recipeDetail.preparationMinutes + recipeDetail.cookingMinutes}    Min   -</span>
+  <span>  
+  
+  
+  
+  Min   -</span>
               <span>${data[i].likes}</span>
               <i class="fa-regular fa-heart checked"></i>  
 
@@ -93,14 +93,11 @@ const cards = (data) => {
       
   
       `
-      );
-    }
- 
+    );
+  }
+
   // recipeDetailsEventListener(data);
 };
-
-
-
 
 /* Recipe Details Site */
 const recipeDetails = (data) => {
@@ -109,21 +106,19 @@ const recipeDetails = (data) => {
   cardContainer.classList.add("makeDisplayNone");
 
   const mainElement = document.querySelector("main");
-   mainElement.classList.remove("makeDisplayNone");
-  
+  mainElement.classList.remove("makeDisplayNone");
+
   const test = document.querySelector("main");
-  
-  
+
   /* --- Created Elements --- */
   const backButtonDiv = document.createElement("div");
   backButtonDiv.setAttribute("class", "arrow-left");
-  
-  const recipeDetailsCon = document.createElement('div');
+
+  const recipeDetailsCon = document.createElement("div");
   recipeDetailsCon.setAttribute("class", "recipeDetailsCon");
 
-
-   const arrowLeftIcon = document.createElement("i");
-   arrowLeftIcon.setAttribute("class", "fa-regular fa-left");
+  const arrowLeftIcon = document.createElement("i");
+  arrowLeftIcon.setAttribute("class", "fa-regular fa-left");
 
   const backButton = document.createElement("button");
   backButton.setAttribute("class", "btn btn-primary btn-sm");
@@ -132,81 +127,77 @@ const recipeDetails = (data) => {
   const recipeImageCon = document.createElement("div");
   recipeImageCon.setAttribute("class", "col col-md-4 recipeImageCon");
 
-const recipeTextCon = document.createElement("div");
-recipeTextCon.setAttribute("class", "col col-md-8 recipeTextCon");
+  const recipeTextCon = document.createElement("div");
+  recipeTextCon.setAttribute("class", "col col-md-8 recipeTextCon");
 
   const recipeIngredientsCon = document.createElement("div");
   recipeIngredientsCon.setAttribute("class", "block recipeIngredientsCon");
 
-const recipeHeader = document.createElement("div");
-recipeHeader.setAttribute("class", "recipeHeader");
+  const recipeHeader = document.createElement("div");
+  recipeHeader.setAttribute("class", "recipeHeader");
 
-const preparationTimeCon = document.createElement("div");
+  const preparationTimeCon = document.createElement("div");
   preparationTimeCon.setAttribute("class", "preparationTimeCon");
-  
+
   const timeIcon = document.createElement("div");
   timeIcon.setAttribute("class", "timeIcon");
-  
+
   const timeIconI = document.createElement("i");
   timeIconI.setAttribute("class", "fa-solid fa-clock");
-  
-const timeTextCon = document.createElement("div");
-timeTextCon.setAttribute("class", "timeText");
 
-const timeTextP = document.createElement("p");
-timeTextP.setAttribute("class", "bold");
-timeTextP.innerText = "Make it in: 35 minutes";
+  const timeTextCon = document.createElement("div");
+  timeTextCon.setAttribute("class", "timeText");
 
+  const timeTextP = document.createElement("p");
+  timeTextP.setAttribute("class", "bold");
+  timeTextP.innerText = "Make it in: 35 minutes";
 
+  const IngredientsSubtitleCon = document.createElement("div");
+  IngredientsSubtitleCon.setAttribute("class", "subtitle");
 
-const IngredientsSubtitleCon = document.createElement("div");
-IngredientsSubtitleCon.setAttribute("class", "subtitle");
+  const IngredientsSubtitleH2 = document.createElement("h2");
+  IngredientsSubtitleH2.innerText = "Ingredients";
 
-const IngredientsSubtitleH2 = document.createElement("h2");
-IngredientsSubtitleH2.innerText = "Ingredients";
+  const IngredientsTextCon = document.createElement("div");
+  IngredientsTextCon.innerText = "Ingredients Textes";
 
-const IngredientsTextCon = document.createElement("div");
-IngredientsTextCon.innerText = "Ingredients Textes";
+  const IngredientsOl = document.createElement("ol");
 
-const IngredientsOl = document.createElement("ol");
-
-const recipeH1 = document.createElement("h1");
+  const recipeH1 = document.createElement("h1");
   recipeH1.textContent = recipeDetail.title;
-  
-const mainImg = document.createElement("img");
-mainImg.setAttribute("alt", "mainImage");
-mainImg.setAttribute("src", recipeDetail.image);
 
-const stepsCon = document.createElement("div");
-stepsCon.setAttribute("class", "block stepsCon");
+  const mainImg = document.createElement("img");
+  mainImg.setAttribute("alt", "mainImage");
+  mainImg.setAttribute("src", recipeDetail.image);
 
-const stepsSubtitle = document.createElement("div");
-stepsSubtitle.setAttribute("class", "subtitle");
+  const stepsCon = document.createElement("div");
+  stepsCon.setAttribute("class", "block stepsCon");
 
-const subtitleH2 = document.createElement("h2");
-subtitleH2.innerText = "Steps";
+  const stepsSubtitle = document.createElement("div");
+  stepsSubtitle.setAttribute("class", "subtitle");
 
-const stepsListCon = document.createElement("div");
-stepsListCon.setAttribute("class", "stepsListCon");
+  const subtitleH2 = document.createElement("h2");
+  subtitleH2.innerText = "Steps";
 
-const stepsListOl = document.createElement("ol");
+  const stepsListCon = document.createElement("div");
+  stepsListCon.setAttribute("class", "stepsListCon");
 
-const moreButton = document.createElement("button");
-moreButton.setAttribute("class", "btn btn-info");
-moreButton.setAttribute("id", "showMoreBtn");
-moreButton.innerText = "Show More";
+  const stepsListOl = document.createElement("ol");
 
+  const moreButton = document.createElement("button");
+  moreButton.setAttribute("class", "btn btn-info");
+  moreButton.setAttribute("id", "showMoreBtn");
+  moreButton.innerText = "Show More";
 
-
-mainElement.appendChild(recipeDetailsCon)
-     recipeDetailsCon.appendChild(backButtonDiv);
-     backButtonDiv.appendChild(backButton);
-     backButtonDiv.appendChild(arrowLeftIcon);
-     recipeDetailsCon.appendChild(recipeImageCon);
-     recipeImageCon.appendChild(mainImg);
-     recipeDetailsCon.appendChild(recipeTextCon);
+  mainElement.appendChild(recipeDetailsCon);
+  recipeDetailsCon.appendChild(backButtonDiv);
+  backButtonDiv.appendChild(backButton);
+  backButtonDiv.appendChild(arrowLeftIcon);
+  recipeDetailsCon.appendChild(recipeImageCon);
+  recipeImageCon.appendChild(mainImg);
+  recipeDetailsCon.appendChild(recipeTextCon);
   recipeTextCon.appendChild(recipeHeader);
-  
+
   recipeTextCon.appendChild(recipeIngredientsCon);
   recipeTextCon.appendChild(timeTextCon);
   recipeTextCon.appendChild(preparationTimeCon);
@@ -265,22 +256,18 @@ mainElement.appendChild(recipeDetailsCon)
   backButton.addEventListener("click", goHome);
 };
 
-
-
 const showMoreFunc = () => {
   let showMoreBtn = document.getElementById("showMoreBtn");
   console.log("showMoreBtn: ", showMoreBtn);
 
+  document.querySelector(".stepsListCon").classList.toggle("stepsListMore");
 
-    document.querySelector(".stepsListCon").classList.toggle("stepsListMore");
+  const stepsListCon = document.querySelector(".stepsListCon");
 
-    const stepsListCon = document.querySelector(".stepsListCon");
-
-    // --- Show more or less textes at the bottom
-    stepsListCon.classList.contains("stepsListMore")
-      ? (showMoreBtn.textContent = "Show Less")
-      : (showMoreBtn.textContent = "Show More");
-
+  // --- Show more or less textes at the bottom
+  stepsListCon.classList.contains("stepsListMore")
+    ? (showMoreBtn.textContent = "Show Less")
+    : (showMoreBtn.textContent = "Show More");
 };
 
 const goHome = () => {
@@ -291,46 +278,84 @@ const goHome = () => {
   recipeDetailsCon.parentNode.removeChild(recipeDetailsCon);
 };
 
-
-
-/* ----------- Add event listener --------------- */
+/* ----------- Add event listener --------------- starts*/
 const addEventListener = (data) => {
-  console.log("data: ", data);
   /* ------- Searching recipes by typing ------------ */
-  function searchFunc(data) {
-    const searchInput = document.getElementById("searchInput");
-
-    return searchInput.addEventListener("input", (e) => {
-      /* multiple spaces trimmed */
-      let inputValue = e.target.value.trim().replace(/  +/g, " ").toUpperCase();
-
-      const searchResult = data.filter((item) =>
-        item.title.toUpperCase().includes(inputValue)
-      );
-      // return cards(searchResult);
-
-      checkBoxLikes(searchResult)
-      //  printData(searchResult);
-      cards(searchResult);
-      // return checkBoxLikes(searchResult);
-    });
-  }
+  const searchInput = document.getElementById("searchInput");
+  searchInput.addEventListener("input", (e) =>searchFunc(data));
+   
+  
   /* ------- Sorting recipes by checkboxing ------------ */
-
-  function checkBoxLikes(data) {
     const checkBLikes = document.querySelector("#checkBoxLikes");
-    const sortCheckBoxLabel = document.querySelector(".sortCheckBoxLabel");
+    checkBLikes.addEventListener("click", () => checkBoxLikesFunk(data));
 
-    checkBLikes.addEventListener("click", (e) => {
-      const isChecked = e.target.checked;
+  
+  /* --- Recipe Event Listener / Open the Recipe Details Site  -----*/
+  const recipeDetailsEventListener = (data) => {
+    const card = document.querySelectorAll(".card");
+
+    card.forEach((item) => {
+      item.addEventListener("click", () => {
+        console.log("click eventListerner");
+
+        recipeDetails(data);
+      });
+    });
+  };
+
+  /* ---------- Select an Option -------------- */
+    const select = document.querySelector(".option-select");
+    select.addEventListener(
+      "change",
+      () => selectOptions(data)
+      // combinedFilters()
+    );
+
+  // searchFunc(data);
+  checkBoxLikesFunk(data);
+  recipeDetailsEventListener(data);
+  // selectOptions(data);
+};
+/* ----------- Add event listener --------------- ends*/
+
+
+const searchFunc = (data) => {
+  const inputValue = document
+    .getElementById("searchInput")
+    .value.trim()
+    .replace(/  +/g, " ")
+    .toUpperCase();
+
+  /* multiple spaces trimmed */
+  //  let inputValue = e.target.value.trim().replace(/  +/g, " ").toUpperCase();
+
+  const searchResult = data.filter((item) =>
+    item.title.toUpperCase().includes(inputValue)
+  );
+  cards(searchResult);
+  // return cards(searchResult);
+
+  // checkBoxLikes(searchResult);
+  //  printData(searchResult);
+  // return checkBoxLikes(searchResult);
+};
+
+
+function checkBoxLikesFunk(data) {
+  // const checkBLikes = document.querySelector("#checkBoxLikes");
+  const sortCheckBoxLabel = document.querySelector(".sortCheckBoxLabel");
+
+      // const isChecked = e.target.checked;
+      const isChecked = sortCheckBoxLabel.checked;
+      console.log("isChecked: ", isChecked);
       let sortedData = [];
-      
+
       data.forEach((item) => {
         sortedData.push(item);
         item;
       });
 
-      if (isChecked ) {
+      if (isChecked) {
         sortCheckBoxLabel.innerHTML = "";
         sortCheckBoxLabel.innerHTML = "Sort by Likes";
 
@@ -353,12 +378,55 @@ const addEventListener = (data) => {
       cards(sortedData);
       // printData(sortedData);
       // recipeDetailsEventListener(data);
+
+
+
+
+
+}
+
+const getDataForOptions = (data) => {
+  const select = document.querySelector(".option-select");
+
+  let response = [];
+
+  data.forEach((item) => {
+    item.badges.forEach((el) => {
+      response.push(el);
     });
+  });
+  const removeDoubbles = [...new Set(response)];
+
+  removeDoubbles.forEach((item) => {
+    const option = document.createElement("option");
+    option.innerText = item.toUpperCase().replace("_", " ");
+    select.appendChild(option);
+  });
+}
+const selectOptions = (data) => {
+  const select = document.querySelector(".option-select");
+
+
+   let result =[]
+  for (let index = 0; index < data.length; index++){
+    console.log("data.badges: ", typeof data[index].badges);
+    for (let i = 0; i < data[index].badges.length; i++) {
+
+
+      if (data[index].badges[i].toUpperCase().replace("_", " ")  === select.value) {
+  result.push(data[index])
+}
+      
+    }
   }
+  console.log("result: ", result);
+  cards(result)
+}
 
-  /* Reseting/ deleting the actuell filters */
-  const deleteFilterButton = (data) => {
+  
 
+/* Reseting/ deleting the actuell filters */
+  const deleteFilterButtonFunk = (data) => {
     const inputGroup = document.querySelector(".input-group");
     const deleteFilterButton = document.createElement("button");
 
@@ -374,35 +442,22 @@ const addEventListener = (data) => {
     });
   };
 
-  /* --- Recipe Event Listener / Open the Recipe Details Site  -----*/
-  const recipeDetailsEventListener = (data) => {
-    console.log("data recipeDetailsEventListener: ", data);
-    const card = document.querySelectorAll(".card");
 
-    card.forEach((item) => {
-      item.addEventListener("click", () => {
-        console.log("click eventListerner");
 
-        recipeDetails(data);
-      });
-    });
-  };
 
-  searchFunc(data)
-  checkBoxLikes(data)
-  deleteFilterButton(data)
-  recipeDetailsEventListener(data)
-}
+// const combinedFilters = (data) => {
+//   console.log(dropdowvalue);
+//   // select dropdown  const dropdowvalue = document.getlementbyId.value;
+//   //select check boxes const dropDownValue = checkboxesCheck.value;
 
+//   const filteredElements = data.filter((element) => {
+//     return( element.name === dropdownvalue || dropdownValue === "all") && (checBoxesvalue.includes(element.name) || checboxes.lenght === 0)
+//   });
+
+// }
 
 // cards(data)
 // fetchData()
 controller();
 
 
-
-// console.log(navigator.geolocation.getCurrentPosition(showPosition))
-
-// function showPosition(){
-//   let latid = position.coords.
-// }
